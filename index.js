4a6daf7d54aa27af7665af76f694cf4dc8b94c5f
Sindre S. Kjær <sindre@sindrekjr.no>
2021-09-26 14:33:49 +0200

    refactor(project): renamed

--- index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const express = require('express');
-
-const { PORT = 9000 } = process.env;
-const app = express();
-
-app.use(express.static(__dirname + '/build'));
-app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
--- src/App/Header.tsx
+++ src/App/Header.tsx
@@ -5,8 +5,30 @@ import { rem } from 'polished';
 const Heading = styled.h1`
   color: #e6e2ad;
   font-size: ${rem(60)};
+  text-align: center;
   -webkit-text-stroke-width: ${rem(2.5)};
   -webkit-text-stroke-color: #290a0a;
+
+  @media (max-width: 1200px) {
+    font-size: ${rem(46)};
+    -webkit-text-stroke-width: ${rem(2)};
+  }
+`;
+
+const HeaderWrapper = styled.header`
+  ${Heading}:last-child {
+      display: none;
+    }
+
+  @media (max-width: 768px) {
+    ${Heading}:first-child {
+      display: none;
+    }
+
+    ${Heading}:last-child {
+      display: flex;
+    }
+  }
 `;
 
 export interface HeaderProps {
@@ -14,9 +36,14 @@ export interface HeaderProps {
 }
 
 export const Header: FC<HeaderProps> = ({
-  title = 'Pretty Good Pathfinder Character Builder',
-}) => (
-  <header>
-    <Heading>{title}</Heading>
-  </header>
-);
+  title = 'Pretty Good Pathfinder Character Build Calculator',
+}) => {
+  const shortTitle = title.split(' ').reduce((short, word) => short + word[0], '');
+
+  return (
+    <HeaderWrapper>
+      <Heading>{title}</Heading>
+      <Heading>{shortTitle}</Heading>
+    </HeaderWrapper>
+  );
+};
